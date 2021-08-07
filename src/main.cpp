36c5d42083bf068a651eeab65079aa2cd50b5bcd
erickstm87 <dev@terickson.io>
2021-08-07 08:40:42 -0600

    tik tak toe

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,11 @@ void printBoard();
 void playerChoose();
 bool updateBoard(int row, int column, string playerType);
 int columnConversion(int columnSelection);
-// bool checkWinner();
-bool gameOver = false;
 int boardFull = 0;
+bool gameOver = false;
+bool checkRow(string playerMark);
+bool checkColumn(string playerMark);
+bool checkDiagonal(string playerMark);
 
 int main() {
     printBoard();
@@ -37,35 +39,44 @@ void printBoard() {
 
 void playerChoose() {
     bool X = true;
-    string playerMark;
-    string player1, player2;
+    string playerMark, playerTurn;
     bool successfulTurn = false;
     while(boardFull < 9) {
         int rowChoice, columnChoice;
         if(X) {
-          playerMark = "X"
-          cout << "player1 which row?" << endl;
-          cin >> rowChoice;
-          cout << "player1 which column?" << endl;
+          playerMark = "X";
+          playerTurn = "player1";
         }
         else {
-          playerMark = "O"
-          cout << "player2 which row?" << endl;
-          cin >> rowChoice;
-          cout << "player2 which column?" << endl;
+          playerMark = "O";
+          playerTurn = "player2";
         }
+        cout << playerTurn << " which row?" << endl;
+        cin >> rowChoice;
+        cout << playerTurn << " which column?" << endl;
         cin >> columnChoice;
-        columnChoice = columnConversion(columnChoice);
+        if(rowChoice > 3 || rowChoice < 1 || columnChoice > 3 || columnChoice < 1) {
+            cout << "please enter valid values" << endl;
+            continue;
+        }
+        columnChoice = columnConversion(columnChoice); // needed to account for weird position given | and _
         successfulTurn = updateBoard(rowChoice, columnChoice, playerMark);
         if(successfulTurn) {
           X = !X; // flip the player from 1 to 2
           boardFull++; // only increment the turn if it was a valid selection
+          checkRow(playerMark);
+          checkColumn(playerMark);
+          checkDiagonal(playerMark);
         }
-        else {
-            cout << "please enter valid values" << endl;
-            continue;
+        
+        if(gameOver) {
+            X = !X;
+            cout << "Game over " << playerTurn << " won" << endl;
+            return;
         }
     }
+    cout << "cat's game you both lost" << endl;
+    
 }
 
 bool updateBoard(int row, int column, string playerType) {
@@ -74,7 +85,6 @@ bool updateBoard(int row, int column, string playerType) {
     row-=1;
     if((boardRows[row][column] == "_" || boardRows[row][column] == " ") && row <= 2) {
         boardRows[row][column] = playerType;
-
         printBoard();
         goodTurn = true;
     }
@@ -103,7 +113,49 @@ int columnConversion(int columnSelection) {
     return columnSelection;
 }
 
-// bool checkWinner() {
-//     if()
-//     return false;
-// }
+bool checkRow(string playerMark) { 
+  int counter;
+  for(int i = 0; i < boardRows.size(); i++) {
+    counter = 0;
+    for(int j = 0; j < boardRows[i].size(); j++) {
+        if(boardRows[i][j] == playerMark) {
+            counter += 1;
+        }
+        if(counter == 3) {
+            gameOver = true;
+        }
+    }
+  }
+  return gameOver;
+}
+
+bool checkColumn(string playerMark) {
+  int counter = 0;
+  for(int i = 0; i < boardRows.size(); i++) {
+      if(boardRows[i][0] == playerMark) {
+          counter += 1;
+      }
+      else if(boardRows[i][1] == playerMark) {
+          counter += 1;
+      }
+      else if(boardRows[i][2] == playerMark) {
+          counter += 1;
+      }
+  }
+  if(counter == 3) {
+      gameOver = true;
+  }
+  return gameOver;
+}
+
+bool checkDiagonal(string playerMark) {
+  // check left to right
+  if(boardRows[0][0] == playerMark && boardRows[1][2] == playerMark && boardRows[2][4] == playerMark) {
+      gameOver = true;
+  }
+  // check right to left
+  else if(boardRows[0][4] == playerMark && boardRows[1][2] == playerMark && boardRows[2][0] == playerMark) {
+      gameOver = true;
+  }
+  return gameOver;
+}
